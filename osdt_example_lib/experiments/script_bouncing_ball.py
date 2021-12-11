146293402211fd4f8974437b6803b235b2c90d70
be3short <beshort@ucsc.edu>
2021-12-11 02:06:20 -0800

    checking in

--- osdt_example_lib/experiments/script_bouncing_ball.py
+++ osdt_example_lib/experiments/script_bouncing_ball.py
@@ -7,18 +7,8 @@ import osdt as dt
 
 from random import random
 
-def main(y_position=1.0,
-        y_velocity=0.0,
-        gravity=9.81,
-        restitution=.95,
-        t=5.0,
-        j=20):
 
-    ball1 = osdt.scripting.run_task("tasks/test.yaml","ball1")
-    ball2 = osdt.scripting.run_task("tasks/test.yaml","ball2")
-
-    dt.run(t=t, j=j)
-
+def plot():
     # create a figure
     fig = dt.create_figure(layout=[[1,3,4,4], [2,3,4,4]],title="Bouncing Ball",
                            width=1600, height=600)
@@ -27,9 +17,19 @@ def main(y_position=1.0,
     fig.subplot(2).plot("y_velocity",max_points=200)
     fig.subplot(3).plot(["y_position", "y_velocity"],max_points=200)
     fig.subplot(4).plot(x="y_velocity", y="y_position",max_points=200)
-    #fig.export("output/single_bouncing_ball/figure.pdf",format="pdf")
-    print(dt.toolbox.global_operator.integrator._integrator.__dict__)
     dt.display()
+def main(action_file="actions/bouncing_ball.yaml"):
+
+    ball1 = osdt.scripting.run_task(action_file,"create_ball")
+    osdt.scripting.run_task(action_file,"edit_config")
+    dt.run()
+
+    plot()
+
+
+    ball1 = osdt.scripting.run_task(action_file,"save_env")
+
+
 
 
 if __name__ == "__main__":
