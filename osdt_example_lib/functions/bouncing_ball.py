84a76b3c29284dceb9137036b13edb1994a9335e
be3short <beshort@ucsc.edu>
2021-12-11 14:51:11 -0800

    cleaning up

--- osdt_example_lib/functions/bouncing_ball.py
+++ osdt_example_lib/functions/bouncing_ball.py
@@ -1,10 +1,10 @@
 import osdt
 
-def figure1(pos_sensor, vel_sensor):
+
+def ball_sampled_figure(pos_sensor, vel_sensor):
     # create a figure
     fig = osdt.create_figure(layout=[[1, 2]], title="Bouncing Ball with Sensors",
                            width=1600, height=600)
-
     fig.subplot(1).plot("y_position", label="ball y position")
     fig.subplot(1).plot("value", label="sensor y position", sys=osdt.get_system(pos_sensor),
                         field_label=" : ")
@@ -12,3 +12,12 @@ def figure1(pos_sensor, vel_sensor):
     fig.subplot(2).plot("value", label="sensor y velocity", sys=osdt.get_system(vel_sensor),
                         color="red")
 
+
+def ball_figure():
+    # create a figure
+    fig = osdt.create_figure(layout=[[1,3,4,4], [2,3,4,4]],title="Bouncing Ball",
+                           width=1600, height=600)
+    fig.subplot(1).plot("y_position",max_points=200)
+    fig.subplot(2).plot("y_velocity",max_points=200)
+    fig.subplot(3).plot(["y_position", "y_velocity"],max_points=200)
+    fig.subplot(4).plot(x="y_velocity", y="y_position",max_points=200)
