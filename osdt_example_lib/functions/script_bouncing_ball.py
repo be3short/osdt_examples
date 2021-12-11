dd4b9fd1387d1037c32a9a971cfc199c29f46d11
be3short <beshort@ucsc.edu>
2021-12-11 02:31:26 -0800

    refactoring

--- osdt_example_lib/functions/script_bouncing_ball.py
+++ osdt_example_lib/functions/script_bouncing_ball.py
@@ -12,21 +12,18 @@ def plot():
     # create a figure
     fig = dt.create_figure(layout=[[1,3,4,4], [2,3,4,4]],title="Bouncing Ball",
                            width=1600, height=600)
-
     fig.subplot(1).plot("y_position",max_points=200)
     fig.subplot(2).plot("y_velocity",max_points=200)
     fig.subplot(3).plot(["y_position", "y_velocity"],max_points=200)
     fig.subplot(4).plot(x="y_velocity", y="y_position",max_points=200)
     dt.display()
-def main(action_file="actions/bouncing_ball.yaml"):
+
+def main(action_file="operators/bouncing_ball.yaml"):
 
     ball1 = osdt.scripting.run_task(action_file,"create_ball")
     osdt.scripting.run_task(action_file,"edit_config")
     dt.run()
-
     plot()
-
-
     ball1 = osdt.scripting.run_task(action_file,"save_env")
 
 
