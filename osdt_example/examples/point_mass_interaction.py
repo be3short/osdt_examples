4821f2998714d6b1764e1d3dbbee5cc0748dba53
be3short <beshort@ucsc.edu>
2021-12-30 17:24:48 -0800

    added point mass

---
+++ osdt_example/examples/point_mass_interaction.py
@@ -0,0 +1,55 @@
+import sys, os
+
+import osdt
+
+from osdt_example.models import point_mass as pm
+import osdt
+V_0=.1
+state1 = pm.State(x_position=1000.0, y_position=1000.0, x_velocity=-V_0, y_velocity = V_0)
+params1 = pm.Params(mass=1100020100.0)
+system1 = osdt.create_system(x=state1, c=pm.C, f=pm.F, u=pm.U, vars={pm.PARAMS: params1}, id="small mass 1")
+
+
+state2 = pm.State(x_position=-1000.0, y_position=-1000.0, x_velocity=V_0, y_velocity = -V_0)
+params2 = pm.Params(mass=1100020110.0)
+system2 = osdt.create_system(x=state2, c=pm.C, f=pm.F, u=pm.U, vars={pm.PARAMS: params2}, id="small mass 2")
+
+
+state3 = pm.State(x_position=1000.0, y_position=-1000.0, x_velocity=V_0, y_velocity = V_0)
+params3 = pm.Params(mass=1100020100.0)
+system3 = osdt.create_system(x=state3, c=pm.C, f=pm.F, u=pm.U, vars={pm.PARAMS: params3}, id="small mass 3")
+
+state4 = pm.State(x_position=-1000.0, y_position=1000.0, x_velocity=-V_0, y_velocity = -V_0)
+params4 = pm.Params(mass=1100020100.0)
+system4 = osdt.create_system(x=state4, c=pm.C, f=pm.F, u=pm.U, vars={pm.PARAMS: params4}, id="small mass 4")
+
+state0 = pm.State(x_position=0.1, y_position=0.1, x_velocity=0.0, y_velocity = 0.0)
+params0 = pm.Params(mass=911000220100.0)
+system0 = osdt.create_system(x=state0, c=pm.C, f=pm.F, u=pm.U, vars={pm.PARAMS: params0}, id="large mass")
+
+pm.connect_all_masses()
+osdt.set_integrator(osdt.get_configuration().integrator_type, max_step=100.0)
+osdt.run(time=100000.0, jumps=20)
+xyfig = osdt.create_figure(width=800, height=600, layout=[[1]], title="Point Mass Interaction",dpi=130)
+xyfig.plot(1,x="x_position",y="y_position")
+#xyfig.plot(1,"y_position")
+
+osdt.display()
+'''
+# create a figure
+fig = osdt.create_figure(layout=[[1,3,4,4], [2,3,4,4]],width=1200, height=600,
+                         title="Bouncing Ball",dpi=130)
+fig.configure_subplot(1, )
+fig.subplot(1).plot("y_position", max_points=200)
+fig.subplot(2).plot("y_velocity", max_points=200)
+fig.subplot(3).plot(["y_position", "y_velocity"], max_points=200)
+fig.subplot(4).plot(x="y_velocity", y="y_position", max_points=200)
+
+# create a figure
+fig2 = osdt.create_figure(width=800, height=600, layout=[[1],[2]], title="Bouncing Ball",dpi=130)
+fig2.configure_subplot(1, x_axis="Time (s)", y_axis="Position (m)")
+fig2.configure_subplot(2, x_axis="Time (s)", y_axis="Velocity (m/s)")
+fig2.plot(1,"y_position")
+fig2.plot(2,"y_velocity")
+osdt.display()
+'''
---
+++ osdt_example/models/point_mass.py
@@ -0,0 +1,142 @@
+import osdt
+import math
+
+ALL_MASSES = "ALL_MASSES"
+PARAMS = "PARAMS"
+class State:
+    def __init__(self,x_position=0.0, y_position=0.0, x_velocity=0.0, y_velocity = 0.0):
+        self.x_position = x_position
+        self.y_position = y_position
+        self.x_velocity = x_velocity
+        self.y_velocity= y_velocity
+
+class Params:
+    def __init__(self, mass = 1.0 ):
+        self.mass = mass
+        self.gravity=0.00000000006674
+
+def C(x, hs):
+
+    return True
+
+
+def F(x, x_dot, sys):
+    point_masses=sys.get_input()
+    x_force_total = 0
+    y_force_total = 0
+
+    for point_mass in point_masses:
+        # define other point mass state as y
+        y = point_mass.x()
+        distance = math.sqrt((x.x_position - y.x_position) * (x.x_position - y.x_position)
+                             + (x.y_position - y.y_position) * (x.y_position - y.y_position));
+
+        x_distance = y.x_position - x.x_position;
+        y_distance = y.y_position - x.y_position;
+        angle = math.atan2(y_distance, x_distance);
+        if distance < 50: distance = 50.0
+        force = sys.get(PARAMS).gravity * (sys.get(PARAMS).mass * point_mass.get(PARAMS).mass) / math.pow(distance, 2)
+        x_force = force * math.cos(angle);
+        y_force = force * math.sin(angle);
+        x_force_total = x_force_total + x_force
+        y_force_total = y_force_total + y_force
+
+    x_dot.x_velocity = x_force_total / sys.get(PARAMS).mass
+    x_dot.y_velocity = y_force_total / sys.get(PARAMS).mass
+    x_dot.x_position = x.x_velocity
+    x_dot.y_position = x.y_velocity
+   # print("envtime="+str(osdt.get_environment_time())+" "+str(x.__dict__))
+
+def CEarth(x, sys):
+    system = sys.connection(id=sys.get(PARAMS).mass_connection_key)
+    earth = system.x()
+    dist = compute_distance(x, earth)
+    if dist < 6361000:# and x.y_velocity==0.0:
+        return False
+    else:
+        return True
+
+
+def D(x,sys):
+    system=sys.connection(id=sys.get(PARAMS).mass_connection_key)
+    earth=system.x()
+    dist=compute_distance(x,earth)
+    if dist < 6361000 and x.y_velocity>0:
+        return True
+    else:
+        return False
+
+
+def G(x,x_plus,sys):
+    system=sys.connection(id=sys.get(PARAMS).mass_connection_key)
+    earth=system.x()
+    dist=compute_distance(x,earth)
+    #if dist < 6361000:
+    print("jump")
+    x_plus.x_velocity=0.0
+    x_plus.y_velocity=0.0
+    x_plus.x_position=x.x_position*.98
+    x_plus.y_position =x.y_position*.98
+
+
+def U(x, sys, *args, **argmap):
+    # get all other point masses
+    point_masses = sys.get(ALL_MASSES)
+    return point_masses
+
+
+
+
+def compute_distance(x,y):
+    distance = math.sqrt((x.x_position - y.x_position) * (x.x_position - y.x_position)
+                         + (x.y_position - y.y_position) * (x.y_position - y.y_position));
+    return distance
+
+
+def connect_all_masses():
+    all_point_masses = []
+    for system in osdt.get_systems().values():
+        system: osdt.system.System=system
+        if type(system.x()) == State:
+            all_point_masses.append(system)
+    for pointmass in all_point_masses:
+        other_masses = []
+        for other_mass in all_point_masses:
+            if other_mass != pointmass:
+                other_masses.append(other_mass)
+        pointmass.set(ALL_MASSES,other_masses)
+
+
+def FDrag(x,x_dot,sys):
+    point_masses = sys.input()
+    x_force_total = 0
+    y_force_total = 0
+    system = sys.connection(id=sys.get(PARAMS).mass_connection_key)
+    earth = system.x()
+    dist = compute_distance(x, earth)
+   # if dist >= 6350000:
+    for point_mass in point_masses:
+        # define other point mass state as y
+        y = point_mass.x()
+        distance = math.sqrt((x.x_position - y.x_position) * (x.x_position - y.x_position)
+                             + (x.y_position - y.y_position) * (x.y_position - y.y_position));
+
+        x_distance = y.x_position - x.x_position;
+        y_distance = y.y_position - x.y_position;
+        angle = math.atan2(y_distance, x_distance);
+        if distance < 5: distance = 5
+        force = sys.get(PARAMS).gravity * (sys.get(PARAMS).mass * point_mass.get(PARAMS).mass) / math.pow(distance, 2)
+        x_force = force * math.cos(angle);
+        y_force = force * math.sin(angle);
+        x_force_total = x_force_total + x_force
+        y_force_total = y_force_total + y_force
+
+    x_force_total = x_force_total
+    y_force_total = y_force_total
+
+
+
+    x_dot.x_velocity = (x_force_total) / sys.get(PARAMS).mass
+    x_dot.y_velocity = y_force_total / sys.get(PARAMS).mass
+    x_dot.x_position = x.x_velocity
+    x_dot.y_position = x.y_velocity
