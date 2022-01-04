c1b3afbb0dc0fedd3a287645aa8e69d528e734f7
be3short <beshort@ucsc.edu>
2022-01-04 02:06:01 -0800

    working on new format for components

--- osdt_example/functions/scripttest.py
+++ osdt_example/functions/scripttest.py
@@ -11,6 +11,72 @@ import osdt,sys
 #print(dat)
 
 
+
+FUNCTION_PREFIX= "f:"
+VARIABLE_PREFIX= "v:"
+OBJECT_PREFIX= "o:"
+SEPARATION_SPLITTER = ":"
+PREFIXES=[FUNCTION_PREFIX,VARIABLE_PREFIX,OBJECT_PREFIX]
+
+
+
+def fill_object(object_val,objectfile):
+    object_val_filled =object_val
+    if type(object_val) is str:
+        if object_val.startswith(VARIABLE_PREFIX):
+            object_val_filled=get_variable_value(object_val,objectfile)
+        elif object_val.startswith(OBJECT_PREFIX):
+            object_val_filled=get_python_object(object_val,objectfile)
+    elif osdt.utils.is_primitive(object_val):
+        object_val_filled = object_val
+    elif type(object_val) is dict:
+        if len(object_val)==1 and FUNCTION_PREFIX in object_val:
+            object_val_filled = object_val[FUNCTION_PREFIX]
+            object_val_filled=fill_object(object_val_filled,objectfile)
+            object_val_filled=osdt.utils.get_function(object_val_filled)
+          #  object_val[FUNCTION_PREFIX]=object_val_filled
+            object_val_filled = {FUNCTION_PREFIX: object_val_filled}
+        else:
+            final_dict = {}
+            for key in object_val:
+                val =object_val[key]
+                final_val = fill_object(val,objectfile)
+                final_dict[key]=final_val
+            object_val_filled= final_dict
+    elif type(object_val) is list:
+        object_val_filled= handle_list(object_val,objectfile)
+    print(object_val_filled)
+    return object_val_filled
+
+def handle_list(object_val,objectfile):
+    final_list=[]
+    if type(object_val) is list:
+        for list_val in object_val:
+            final_val = fill_object(list_val,objectfile)
+            final_list.append(final_val)
+    return final_list
+
+def get_python_object(object_val, objectfile):
+    print(object_val[len(OBJECT_PREFIX):])
+    return osdt.utils.get_function(object_val[len(OBJECT_PREFIX):])
+def get_variable_value(object_val, objectfile):
+    components = object_val.split(SEPARATION_SPLITTER)
+    parent_key = components[1]
+    parent = objectfile[parent_key]
+    for ind in range(2,len(components)):
+        next_key = components[ind]
+        if next_key in parent:
+            parent=parent[next_key]
+    print(parent)
+    final_val=fill_object(parent,objectfile)
+    return final_val
+
+
+
+
+
+
+
 def call_script(path,args):#"osdt_example/testargs.yaml"):
     #argzs=osdt.load_objfile(arg_path)["fig_args"]
     ob=osdt.load_objfile(path,args=args)
