d23471ae45763dd000b9fc66dc76acd158c23209
be3short <beshort@ucsc.edu>
2022-01-03 22:40:31 -0800

    better

--- osdt_example/functions/scripttest.py
+++ osdt_example/functions/scripttest.py
@@ -4,8 +4,20 @@ import osdt,sys
 
  #   osdt.utils.create_script_argfile(sys.argv[1],sys.argv[2])
 #else:
-if sys.argv[1] == "make":
-    osdt.utils.create_script_argfile(sys.argv[2], sys.argv[3])
-else:
-    dat=osdt.utils.run_script(*sys.argv[1:])
-print(dat)
+#if sys.argv[1] == "make":
+#    osdt.utils.create_script_argfile(sys.argv[2], sys.argv[3])
+#else:
+#    dat=osdt.utils.run_script(*sys.argv[1:])
+#print(dat)
+
+
+def call_script(path,args):#"osdt_example/testargs.yaml"):
+    #argzs=osdt.load_objfile(arg_path)["fig_args"]
+    ob=osdt.load_objfile(path,args=args)
+    ret_val=ob["return"]
+    osdt.display()
+    return ret_val
+
+
+
+#call_script("osdt_example/test.yaml")
--- osdt_example/functions/simple_timer.py
+++ osdt_example/functions/simple_timer.py
@@ -1,9 +1,12 @@
 import osdt
 
 
-osdt.utils.generate_system_file("test_sys_file","osdt_example.models.simple_timer","State","Params","Params2")
-print(osdt.load_objfile("test_sys_file.yaml"))
-osdt.load_objfile("runtest.yaml")
+#osdt.utils.generate_system_file("test_sys_file","osdt_example.models.simple_timer","State","Params","Params2")
+#print(osdt.load_objfile("test_sys_file.yaml"))
+#osdt.load_objfile("runtest.yaml")
+
+
+
 
 def run():
     print(osdt.get_system("system").get("PARAMS"))
@@ -18,3 +21,27 @@ def timer_figure():
     fig.subplot(1).plot("value")
 
     fig.export("../output/timer_fig",format="png")
+
+def objfile_figure_dict(dictionary):
+    return objfile_figure(**dictionary)
+
+def objfile_figure(figure={}, subplots={}, data={}, export={}, display=True):
+    fig = osdt.create_figure(**figure)
+    for subplot in subplots:
+        fig.configure_subplot(subplot=subplot,**subplots[subplot])
+    for subplot in data:
+        plot_data = data[subplot]
+        for dat in plot_data:
+            fig.plot(subplot, **dat)
+    try:
+        path = export["path"]
+        if path is not None:
+            fig.export(**export)
+    except:
+        pass
+    if not display:
+        fig.close()
+
+def call_func():
+    kwdict = dictionary
+    return kwdict
