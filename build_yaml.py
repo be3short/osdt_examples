4ee50dd3ae837974926c2b4b9a546bf2f80e17ee
be3short <beshort@ucsc.edu>
2022-02-24 09:32:30 -0800

    got building going good!

--- build_yaml.py
+++ build_yaml.py
@@ -9,5 +9,9 @@ from osdt_examples.models import ball,sensor
 
 
 if __name__ == "__main__":
-    #osdt.constructor.create_environment_opfile(systems={"ball":ball.opfile,"pos_sensor":sensor.opfile,"vel_sensor":sensor.opfile},connections=8,integrator=True,run=True,path="test10.yaml")
-    osdt.constructor.build_figure_file(layout=[[1],[2]],args={"pos_system":[],"vel_system":[]},path="fig11")
+    #
+    osdt.constructor.build_figure_file(layout=[[1],[2]],args={"pos_system":[],"vel_system":[]},path="fig12")
+    osdt.constructor.create_environment_opfile(
+        systems={"ball": ball.opfile, "pos_sensor": sensor.opfile,
+                 "vel_sensor": sensor.opfile}, connections=2, integrator=True,
+        run=True,opfile_calls={"fig":"osdt_examples/fig10.yaml"}, path="test13")
