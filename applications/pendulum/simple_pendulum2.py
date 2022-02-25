de3fe7082d5297b43863c1eac5b019314278b072
be3short <beshort@ucsc.edu>
2022-02-24 20:21:48 -0800

    deleted test files

--- applications/pendulum/simple_pendulum2.py
+++ applications/pendulum/simple_pendulum2.py
@@ -3,7 +3,7 @@ import os.path
 import sys
 
 import osdt as dt
-from osdt_examples.models import simple_pendulum
+from osdt_examples.models import simple_pendulum,simple_pendulum2
 
 
 import osdt.constructor
@@ -17,6 +17,9 @@ fb.add_func(None, osdt.run)
 osdt.constructor.build_figure_file(layout=[[1],[2]],path="applications/pendulum/figure1.yaml")
 fb.add_file("figure",osdt.get_path("osdt_examples/applications/pendulum/figure1.yaml"))
 fb.create_opfile("applications/pendulum/pendulum")
+
+
+
 
 dt.display()
 
--- models/simple_pendulum2.py
+++ models/simple_pendulum2.py
@@ -1,6 +1,6 @@
 """Bouncing ball model"""
 import osdt as dt
-import math,figure
+import math
 
 class State():  # state
     def __init__(self, angle=1.0,velocity=0.0):
@@ -19,7 +19,7 @@ class Params():  # parameters
 
 def F(x, x_dot, hs):
     x_dot.angle = x.velocity
-    x.acceleration = -(hs.params.gravity/hs.params.length)*math.sin(x.angle)
+    x.acceleration = -(hs.get(Params).gravity/hs.get(Params).length)*math.sin(x.angle)
     x_dot.velocity = x.acceleration
 
 
@@ -27,33 +27,33 @@ def Y(x, hs, **args):
     if system in args:
         func = hs.outs[system]
     return func(x,hs)
+if __name__ == "__main__":
+    # create the pendulum system
+    system = dt.create_system(State(angle=1.5, velocity=0.0), f=F,
+                              vars={Params:Params(length=1.0, mass=1.0, gravity=9.81)})
 
-# create the pendulum system
-system = dt.create_system(State(angle=1.5, velocity=0.0), f=F,
-                          params=Params(length=1.0, mass=1.0, gravity=9.81))
-
-system2 =  dt.create_system(State(angle=1.5, velocity=0.0), f=F,
-                          params=Params(length=1.0, mass=1.0, gravity=9.81))
-outputs = {system2:get_pendulum_output}
-system2.outs = outputs
-
-
+    system2 =  dt.create_system(State(angle=1.5, velocity=0.0), f=F,
+                              vars={Params:Params(length=1.0, mass=1.0, gravity=9.81)})
+    outputs = {system2:get_pendulum_output}
+    system2.outs = outputs
 
 
-# run the simulation
-dt.run(t=10.0, j=20)
 
-# create a figure
-fig = figure.MultiPlotFigure(layout=[[1],[2]],
-                             title="Pendulum",
-                             width=1600, height=600)
 
-# plot the angle and velocity
-fig.sub(1).plot("angle")
-fig.sub(2).plot("velocity")
+    # run the simulation
+    dt.run(t=10.0, j=20)
 
-# display all figures
-dt.display()
+    # create a figure
+    fig = dt.create_figure(layout=[[1],[2]],
+                                 title="Pendulum",
+                                 width=1600, height=600)
+
+    # plot the angle and velocity
+    fig.sub(1).plot("angle")
+    fig.sub(2).plot("velocity")
+
+    # display all figures
+    dt.display()
 
 
 
